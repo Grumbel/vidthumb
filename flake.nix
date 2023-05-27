1cf3a14190100f4e61fb6b35e77a1221eb26d671
Ingo Ruhnke <grumbel@gmail.com>
2023-05-27 11:37:36 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Video Thumbnailer";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
@@ -11,18 +11,14 @@
 
     logmich.url = "github:logmich/logmich";
     logmich.inputs.nixpkgs.follows = "nixpkgs";
-    logmich.inputs.flake-utils.follows = "flake-utils";
     logmich.inputs.tinycmmc.follows = "tinycmmc";
-
-    gstreamer-fix.url = "github:milahu/nixpkgs?ref=patch-22";
   };
 
-  outputs = { self, nixpkgs, flake-utils, gstreamer-fix,
+  outputs = { self, nixpkgs, flake-utils, # gstreamer-fix,
               tinycmmc, logmich }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        gstreamer-fix-pkgs = gstreamer-fix.legacyPackages.${system};
       in {
         packages = rec {
           default = vidthumb;
@@ -31,21 +27,20 @@
             pname = "vidthumb";
             version = "0.0.0";
 
-            src = nixpkgs.lib.cleanSource ./.;
+            src = ./.;
 
-            nativeBuildInputs = [
-              pkgs.cmake
-              pkgs.pkg-config
+            nativeBuildInputs = with pkgs; [
+              cmake
+              pkg-config
             ];
 
-            buildInputs = [
+            buildInputs = with pkgs; [
+              cairomm
+              fmt_8
+              gst_all_1.gstreamermm
+            ] ++ [
               tinycmmc.packages.${system}.default
               logmich.packages.${system}.default
-
-              pkgs.cairomm
-              pkgs.fmt_8
-              # pkgs.gst_all_1.gstreamermm
-              gstreamer-fix-pkgs.gst_all_1.gstreamermm
             ];
           };
         };
